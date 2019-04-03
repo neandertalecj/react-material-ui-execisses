e2d2647a95dab21f08cce3a9b173ac4f235123ba
neandertalecj <neandertalecj@gmail.com>
2019-04-03 18:31:46 +0300

    Lesson 3. Icons , Dialogs

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import CreateDialog from '../exercises/Dialogs/Create'
 
 export default props =>
             <AppBar position="static">
                 <Toolbar>
-                <Typography variant="headline" color="inherit">
-                    Exercises Database
-                </Typography>
+                    <Typography variant="headline" color="inherit" style={{flex: 1}}>
+                        Exercises Database
+                    </Typography>
+                    <CreateDialog />
                 </Toolbar>
             </AppBar>
  
